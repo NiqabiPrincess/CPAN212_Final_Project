ee1d1e78755cc12d31cdde85d162aba668041ae6
Raj Patel <raj.patel514@gmail.com>
2025-11-29 19:15:47 -0500

    Partials

--- routes/userValidation.js
+++ routes/userValidation.js
@@ -1,7 +1,10 @@
-const Registration = require("../models/Registration");
 const express = require("express");
 const router = express.Router();
+const Registration = require("../models/Registration");
 
+// ----------------------
+// REGISTER ROUTES
+// ----------------------
 router.get("/register", (req, res) => {
     res.render("register", { errors: null });
 });
@@ -14,7 +17,7 @@ router.post("/register", async (req, res) => {
     if (!password) errors.push("Password is required");
 
     if (password && password.length < 4) {
-        errors.push("Password must be at least 4 characters");
+        errors.push("Password must be at least 4 characters long");
     }
 
     if (errors.length > 0) {
@@ -32,4 +35,41 @@ router.post("/register", async (req, res) => {
     res.redirect("/login");
 });
 
+// ----------------------
+// LOGIN ROUTES
+// ----------------------
+router.get("/login", (req, res) => {
+    res.render("login", { errors: null });
+});
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const errors = [];
+
+    if (!email) errors.push("Email is required");
+    if (!password) errors.push("Password is required");
+
+    if (errors.length > 0) {
+        return res.render("login", { errors });
+    }
+
+    const user = await Registration.findOne({ email });
+
+    if (!user) {
+        return res.render("login", { errors: ["Email not found"] });
+    }
+
+    if (user.password !== password) {
+        return res.render("login", { errors: ["Incorrect password"] });
+    }
+
+    req.session.user = {
+        id: user._id,
+        email: user.email
+    };
+
+    res.redirect("/");
+});
+
+// Export router
 module.exports = router;
